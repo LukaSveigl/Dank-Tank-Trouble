b6719fa400ff9bb6eb5d90114446bbd576901eae
LukaSveigl <lukasveigl2708@gmail.com>
2022-06-05 21:51:36 +0200

    Updated data loaders

---
+++ common/engine/data-loader/Loader.js
@@ -0,0 +1,37 @@
+/**
+ * @class Loader - loads properties of objects from JSON files. Use as an interface.
+ */
+export class Loader {
+
+    /**
+     * Constructs a new Loader object.
+     */
+    constructor() {
+        this.json = null;
+        this.jsonUrl = null;
+        this.dirname = null;
+
+        this.cache = new Map();
+    }
+
+    /**
+     * Gets the JSON object at the URL.
+     * @param {string} url - the URL of the JSON file.
+     * @returns the JSON object of the given url.
+     */
+    fetchJson(url) {
+        return fetch(url)
+            .then(response => response.json())
+            .catch(error => console.error(error));
+    }
+
+    /**
+     * Loads the entire JSON file.
+     * @param {string} url - the URL of the JSON file. 
+     */
+    async load(url) {
+        this.jsonUrl = new URL(url, window.location);
+        this.json = await this.fetchJson(url);
+    }
+
+}
